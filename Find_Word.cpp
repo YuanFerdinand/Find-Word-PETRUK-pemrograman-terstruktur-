03a90066cd21984036625275919f19b99615175c
YuanFerdinand <48609239+YuanFerdinand@users.noreply.github.com>
2019-03-20 23:06:54 +0700

    FINALISASI FIX
    Hasil Final Tugas Find Words Pemrograman Terstruktur 2018/2019

--- Find_Word.cpp
+++ Find_Word.cpp
@@ -7,16 +7,16 @@ ABIE PERDANA KUSUMA		1817051054 */
 #include <cstring>
 using namespace std;
 void pengecekan(){
-	char t0[15]		=	{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'};
-	char t1[15]		=	{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'};
-	char t2[15]		=	{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'};
-	char t3[15]		=	{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'};
-	char t4[15]		=	{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'};
-	char t5[15]		=	{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'};
-	char t6[15]		=	{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'};
-	char t7[15]		=	{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'};
-	char t8[15]		=	{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'};
-	char t9[15]		=	{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'};
+	char t0[15]	=	{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'};
+	char t1[15]	=	{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'};
+	char t2[15]	=	{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'};
+	char t3[15]	=	{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'};
+	char t4[15]	=	{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'};
+	char t5[15]	=	{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'};
+	char t6[15]	=	{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'};
+	char t7[15]	=	{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'};
+	char t8[15]	=	{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'};
+	char t9[15]	=	{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'};
 	char t10[15]	=	{'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'};
 	char t11[15]	=	{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'};
 	char t12[15]	=	{'m','o','s','g','z','c','z','d','t','d','b','o','o','t','o'};
@@ -138,13 +138,13 @@ void pengecekan(){
 			}
 		}
 	}
-	//====pengujiannya
+	//Pengujiannya
 	if(coba!=0){
-	cout<<"ADA"<<endl;
-	coba=0;
+		cout<<"ADA"<<endl;
+		coba=0;
 	}	
 	else{
-	cout<<"TIDAK ADA"<<endl;
+		cout<<"TIDAK ADA"<<endl;
 	}
 }
 int main ()
@@ -164,12 +164,12 @@ int main ()
 	char t12[15]	=	{'m','o','s','g','z','c','z','d','t','d','b','o','o','t','o'};
 	char t13[15]	=	{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'};
 	char t14[15]	= 	{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'};
-	char *(table[15]) = {t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14};
+	char *(table[15]) =	{t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14};
 	int cari;
 	// Print Tabel
 	for (int i=0;i<15;i++){
 		for (int j=0;j<15;j++){
-		cout<<*(*(table+i)+j)<<" ";
+			cout<<*(*(table+i)+j)<<" ";
 		}
 	cout<<endl;
 	}
