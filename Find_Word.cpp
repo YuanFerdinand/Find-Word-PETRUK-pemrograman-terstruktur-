b6e56b205f0566a93a2138d4b12bc41d18ff3eb6
eggybintangn13 <48612244+eggybintangn13@users.noreply.github.com>
2019-03-20 22:55:58 +0700

    Penambahan Input kata menjadi n kali

--- Find_Word.cpp
+++ Find_Word.cpp
@@ -25,7 +25,9 @@ void pengecekan(){
 	jumlah=strlen(kata);
 	for(int i=0;i<15;i++){
 		for(int j=0;j<15;j++){
-			if(kata[0]==*(*(table+i)+j)){//====diagonal kanan atas
+			if(kata[0]==*(*(table+i)+j)){
+				
+				// Diagonal kanan atas
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i-k))+(j+k))){
 						uji=k;}
@@ -37,7 +39,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//====diagonal kanan bawah
+				
+				// Diagonal kanan bawah
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i+k))+(j+k))){
 						uji=k;}
@@ -49,7 +52,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//==== kanan
+				
+				// Kanan
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i))+(j+k))){
 						uji=k;}
@@ -61,7 +65,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//==== kiri
+				
+				// Kiri
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i))+(j-k))){
 						uji=k;}
@@ -73,7 +78,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//==== atas
+				
+				//Atas
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i-k))+(j))){
 						uji=k;}
@@ -85,7 +91,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//==== bawah
+				
+				// Bawah
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i+k))+(j))){
 						uji=k;}
@@ -97,7 +104,8 @@ void pengecekan(){
 				else{
 					coba=coba+0;}
 				uji=0;
-				//====diagonal kiri atas
+				
+				// Diagonal kiri atas
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i-k))+(j-k))){
 						uji=k;}
@@ -108,7 +116,9 @@ void pengecekan(){
 					coba=coba+1;}
 				else{
 					coba=coba+0;}
-				uji=0;//====diagonal kiri bawah
+				uji=0;
+				
+				// Diagonal kiri bawah
 				for(int k=0;k<jumlah;k++){
 					if(kata[k]==*(*(table+(i+k))+(j-k))){
 						uji=k;}
@@ -134,16 +144,16 @@ void pengecekan(){
 }
 int main ()
 {
-	char t0[15]		=	{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'};
-	char t1[15]		=	{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'};
-	char t2[15]		=	{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'};
-	char t3[15]		=	{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'};
-	char t4[15]		=	{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'};
-	char t5[15]		=	{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'};
-	char t6[15]		=	{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'};
-	char t7[15]		=	{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'};
-	char t8[15]		=	{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'};
-	char t9[15]		=	{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'};
+	char t0[15]	=	{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'};
+	char t1[15]	=	{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'};
+	char t2[15]	=	{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'};
+	char t3[15]	=	{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'};
+	char t4[15]	=	{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'};
+	char t5[15]	=	{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'};
+	char t6[15]	=	{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'};
+	char t7[15]	=	{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'};
+	char t8[15]	=	{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'};
+	char t9[15]	=	{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'};
 	char t10[15]	=	{'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'};
 	char t11[15]	=	{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'};
 	char t12[15]	=	{'m','o','s','g','z','c','z','d','t','d','b','o','o','t','o'};
@@ -151,11 +161,17 @@ int main ()
 	char t14[15]	= 	{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'};
 	char *(table[15]) = {t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14};
 	int cari;
-	//=====print tabel yang diatas
+	// Print Tabel
 	for (int i=0;i<15;i++){
 		for (int j=0;j<15;j++){
 		cout<<*(*(table+i)+j)<<" ";
 		}
 	cout<<endl;
+	}
+	// Jumlah kata
+	cout<<"Masukkan jumlah kata yang ingin dicari : ";
+	cin>>cari;
+	for(int i=0;i<cari;i++){
+		pengecekan();
 	}
 }
